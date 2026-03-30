0766053fba653c78943aefc158e4e6a9d68992a1
Najika Yoo <najika.skyler.halsema@gmail.com>
2026-03-29 18:18:57 -0700

    chore: rename npc scene

--- npc/dialogue/dialogue_entry.gd
+++ npc/dialogue/dialogue_entry.gd
@@ -1,9 +1,9 @@
 class_name DialogueEntry extends Resource
 
 @export_group("Images")
-@export var expression: Texture = null
-@export var character: Texture = null
+@export var expression: Texture = preload("uid://d000xjtr4iqtk")
+@export var character: Texture = preload("uid://copkt78vgp37i")
 @export_group("Text")
 @export_multiline var text: String = ""
 @export_group("Voice")
-@export var voice: AudioStream = null
+@export var voice: AudioStream = preload("uid://bm2mvov4dm0aw")
